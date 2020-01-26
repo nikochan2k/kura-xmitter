716b069fbaf637a39694e5c21505eeeb2941b67f
Yoshihiro Seki <nikochan2k@gmail.com>
2020-01-26 23:28:06 +0900

    fix bugs of Synchronizer

---
+++ src/Synchronizer.ts
@@ -0,0 +1,191 @@
+import {
+  AbstractAccessor,
+  AbstractFileSystem,
+  FileSystemAsync,
+  FileSystemIndex,
+  FileSystemObject,
+  INDEX_FILE_NAME
+} from "kura";
+
+class Deletion {
+  dstDirectories: string[] = [];
+  dstFiles: string[] = [];
+  srcDirectories: string[] = [];
+  srcFiles: string[] = [];
+}
+
+export class Synchronizer {
+  private dstAccessor: AbstractAccessor;
+  private srcAccessor: AbstractAccessor;
+
+  constructor(public src: FileSystemAsync, public dst: FileSystemAsync) {
+    const srcFS = src.fileSystem as AbstractFileSystem<AbstractAccessor>;
+    this.srcAccessor = srcFS.accessor;
+    if (!this.srcAccessor || !this.srcAccessor.useIndex) {
+      throw new Error(
+        `Source filesystem "${srcFS.name}" has no index "${INDEX_FILE_NAME}"`
+      );
+    }
+
+    const dstFS = dst.fileSystem as AbstractFileSystem<AbstractAccessor>;
+    this.dstAccessor = dstFS.accessor;
+    if (!this.dstAccessor || !this.dstAccessor.useIndex) {
+      throw new Error(
+        `Destination filesystem "${dstFS.name}" has no index "${INDEX_FILE_NAME}"`
+      );
+    }
+  }
+
+  async synchronizeAll() {
+    await this.synchronizeDirectory(this.src.root.fullPath);
+  }
+
+  async synchronizeDirectory(dirPath: string) {
+    const deletion = new Deletion();
+    await this.synchronize(dirPath, deletion);
+
+    for (let i = deletion.srcFiles.length - 1; 0 <= i; i--) {
+      await this.srcAccessor.delete(deletion.srcFiles[i], true);
+    }
+    for (let i = deletion.dstFiles.length - 1; 0 <= i; i--) {
+      await this.srcAccessor.delete(deletion.dstFiles[i], true);
+    }
+    for (let i = deletion.srcDirectories.length - 1; 0 <= i; i--) {
+      await this.srcAccessor.delete(deletion.srcDirectories[i], false);
+    }
+    for (let i = deletion.dstDirectories.length - 1; 0 <= i; i--) {
+      await this.srcAccessor.delete(deletion.dstDirectories[i], true);
+    }
+  }
+
+  private async copyFile(
+    srcAccessor: AbstractAccessor,
+    dstAccessor: AbstractAccessor,
+    obj: FileSystemObject
+  ) {
+    const srcBlob = await srcAccessor.getContent(obj.fullPath);
+    await dstAccessor.putObject(obj, srcBlob);
+  }
+
+  private async synchronize(dirPath: string, deletion: Deletion) {
+    const srcIndex = (await this.srcAccessor.getIndex(dirPath)) || {};
+    const dstIndex = (await this.dstAccessor.getIndex(dirPath)) || {};
+    const index: FileSystemIndex = {};
+
+    const srcKeys = Object.keys(srcIndex);
+    const dstKeys = Object.keys(dstIndex);
+    outer: while (0 < srcKeys.length) {
+      const srcPath = srcKeys.shift();
+      if (!srcPath) {
+        break;
+      }
+      const srcRecord = srcIndex[srcPath];
+      const srcObj = srcRecord.obj;
+      for (let i = 0, end = dstKeys.length; i < end; i++) {
+        const dstPath = dstKeys[i];
+        const dstRecord = dstIndex[dstPath];
+        const dstObj = dstRecord.obj;
+        if (srcPath === dstPath) {
+          if (srcObj.size == null && dstObj.size != null) {
+            // TODO
+            throw new Error("source is directory and destination is file");
+          } else if (srcObj.size != null && dstObj.size == null) {
+            // TODO
+            throw new Error("source is file and destination is directory");
+          }
+
+          const srcDeleted = srcRecord.deleted;
+          const dstDeleted = dstRecord.deleted;
+          if (srcObj.size != null) {
+            if (srcDeleted != null && dstDeleted == null) {
+              deletion.dstFiles.push(dstObj.fullPath);
+              index[srcPath] = srcRecord;
+            } else if (srcDeleted == null && dstDeleted != null) {
+              deletion.srcFiles.push(srcObj.fullPath);
+              index[dstPath] = dstRecord;
+            } else if (srcDeleted != null && dstDeleted != null) {
+              if (srcDeleted <= dstDeleted) {
+                index[srcPath] = srcRecord;
+              } else {
+                index[dstPath] = dstRecord;
+              }
+            } else {
+              const srcUpdated = srcRecord.updated;
+              const dstUpdated = dstRecord.updated;
+              if (srcUpdated === dstUpdated) {
+                if (srcObj.size !== dstObj.size) {
+                  await this.copyFile(
+                    this.srcAccessor,
+                    this.dstAccessor,
+                    srcObj
+                  );
+                }
+                index[srcPath] = srcRecord;
+              } else {
+                if (dstUpdated < srcUpdated) {
+                  await this.copyFile(
+                    this.srcAccessor,
+                    this.dstAccessor,
+                    srcObj
+                  );
+                  index[srcPath] = srcRecord;
+                } else {
+                  await this.copyFile(
+                    this.dstAccessor,
+                    this.srcAccessor,
+                    dstObj
+                  );
+                  index[dstPath] = dstRecord;
+                }
+              }
+            }
+          } else {
+            if (srcDeleted != null && dstDeleted == null) {
+              deletion.dstDirectories.push(dstObj.fullPath);
+              index[srcPath] = srcRecord;
+            } else if (srcDeleted == null && dstDeleted != null) {
+              deletion.srcDirectories.push(srcObj.fullPath);
+              index[dstPath] = dstRecord;
+            } else if (srcDeleted != null && dstDeleted != null) {
+              if (srcDeleted <= dstDeleted) {
+                index[srcPath] = srcRecord;
+              } else {
+                index[dstPath] = dstRecord;
+              }
+            } else {
+              await this.synchronize(srcObj.fullPath, deletion);
+              index[srcPath] = srcRecord;
+            }
+          }
+
+          dstKeys.splice(i);
+          delete dstIndex[dstPath];
+          continue outer;
+        }
+      }
+
+      if (srcObj.size != null) {
+        await this.copyFile(this.srcAccessor, this.dstAccessor, srcObj);
+      } else {
+        await this.dstAccessor.putObject(srcObj);
+        await this.synchronize(srcObj.fullPath, deletion);
+      }
+      index[srcPath] = srcRecord;
+    }
+
+    for (const dstRecord of Object.values(dstIndex)) {
+      const dstObj = dstRecord.obj;
+      const destPath = dstObj.fullPath;
+      if (dstObj.size != null) {
+        await this.copyFile(this.dstAccessor, this.srcAccessor, dstObj);
+      } else {
+        await this.srcAccessor.putObject(dstObj);
+        await this.synchronize(destPath, deletion);
+      }
+      index[destPath] = dstRecord;
+    }
+
+    await this.srcAccessor.putIndex(dirPath, index);
+    await this.dstAccessor.putIndex(dirPath, index);
+  }
+}
---
+++ src/__tests__/synchronize.spec.ts
@@ -0,0 +1,131 @@
+require("fake-indexeddb/auto");
+import { S3 } from "aws-sdk";
+import { Synchronizer } from "../Synchronizer";
+import { FileSystemAsync, IdbLocalFileSystemAsync } from "kura";
+import { S3LocalFileSystemAsync } from "kura-s3";
+
+let local: FileSystemAsync;
+let remote: FileSystemAsync;
+let synchronizer: Synchronizer;
+beforeAll(async () => {
+  const idbLocalFileSystem = new IdbLocalFileSystemAsync(
+    "web-file-system-test",
+    true
+  );
+  local = await idbLocalFileSystem.requestFileSystemAsync(
+    window.PERSISTENT,
+    Number.MAX_VALUE
+  );
+
+  const options: S3.ClientConfiguration = {
+    accessKeyId: "KFS0LZVKZ8G456A502L3",
+    secretAccessKey: "uVwBONMdTwJI1+C8jUhrypvshHz3OY8Ooar3amdC",
+    endpoint: "http://127.0.0.1:9000",
+    s3ForcePathStyle: true, // needed with minio?
+    signatureVersion: "v4"
+  };
+
+  const s3 = new S3(options);
+  const bucket = "web-file-system-test";
+  try {
+    await s3.createBucket({ Bucket: bucket }).promise();
+  } catch (e) {}
+  const list = await s3.listObjectsV2({ Bucket: bucket }).promise();
+  for (const content of list.Contents) {
+    await s3.deleteObject({ Bucket: bucket, Key: content.Key }).promise();
+  }
+
+  const s3LocalFileSystem = new S3LocalFileSystemAsync(
+    options,
+    "web-file-system-test",
+    true
+  );
+  remote = await s3LocalFileSystem.requestFileSystemAsync(
+    window.PERSISTENT,
+    Number.MAX_VALUE
+  );
+
+  synchronizer = new Synchronizer(local, remote);
+});
+
+test("add a empty file", async done => {
+  let localFE = await local.root.getFile("empty.txt", {
+    create: true,
+    exclusive: true
+  });
+  await synchronizer.synchronizeAll();
+
+  localFE = await local.root.getFile("empty.txt");
+  const remoteReader = remote.root.createReader();
+  const remoteEntries = await remoteReader.readEntries();
+  expect(remoteEntries.length).toBe(1);
+  const remoteE = remoteEntries[0];
+  expect(remoteE.isFile).toBe(true);
+  expect(remoteE.name).toBe(localFE.name);
+  expect(remoteE.fullPath).toBe(localFE.fullPath);
+
+  const localMeta = await localFE.getMetadata();
+  const remoteMeta = await remoteE.getMetadata();
+  expect(remoteMeta.size).toBe(localMeta.size);
+
+  done();
+});
+
+test("add a text file", async done => {
+  let localFE = await local.root.getFile("test.txt", {
+    create: true,
+    exclusive: true
+  });
+  const writer = await localFE.createWriter();
+  await writer.write(new Blob(["hoge"], { type: "text/plain" }));
+
+  await synchronizer.synchronizeAll();
+
+  const remoteReader = remote.root.createReader();
+  const remoteEntries = await remoteReader.readEntries();
+  expect(remoteEntries.length).toBe(2);
+
+  let entries = remoteEntries.filter(re => {
+    return re.name === "empty.txt";
+  });
+  expect(entries.length).toBe(1);
+  const emptyTxt = entries[0];
+  expect(emptyTxt.isFile).toBe(true);
+  const emptyTxtMeta = await emptyTxt.getMetadata();
+  expect(emptyTxtMeta.size).toBe(0);
+
+  entries = remoteEntries.filter(re => {
+    return re.name === "test.txt";
+  });
+  expect(entries.length).toBe(1);
+  const testTxt = entries[0];
+  expect(testTxt.isFile).toBe(true);
+  localFE = await local.root.getFile("test.txt");
+  const localFEMeta = await localFE.getMetadata();
+  const testTxtMeta = await testTxt.getMetadata();
+  expect(testTxtMeta.size).toBe(localFEMeta.size);
+
+  done();
+});
+
+test("create folder, and add a text file", async done => {
+  let localDE = await local.root.getDirectory("folder", {
+    create: true,
+    exclusive: true
+  });
+  await localDE.getFile("in.txt", {
+    create: true,
+    exclusive: true
+  });
+
+  await synchronizer.synchronizeAll();
+
+  const localFE = await local.root.getFile("/folder/in.txt");
+  const localMeta = await localFE.getMetadata();
+  const remoteDE = await remote.root.getDirectory("folder");
+  const remoteFE = await remoteDE.getFile("in.txt");
+  const remoteMeta = await remoteFE.getMetadata();
+  expect(remoteMeta.size).toBe(localMeta.size);
+
+  done();
+});
