2328db4146043ed356fca53de23f4d732e1c4d26
Yoshihiro Seki <yoshihiro@YoshihironoMacBook-Pro.local>
2020-07-11 22:20:11 +0900

    modify testcase for error handling

--- src/__tests__/syncronize.ts
+++ src/__tests__/syncronize.ts
@@ -16,154 +16,180 @@ export function testAll(
   });
 
   test("add a empty file, sync all", async (done) => {
-    let localFE = await local.root.getFile("empty.txt", {
-      create: true,
-      exclusive: true,
-    });
-    console.log((await local.root.createReader().readEntries()).length);
-    await synchronizer.synchronizeAll();
+    try {
+      let localFE = await local.root.getFile("empty.txt", {
+        create: true,
+        exclusive: true,
+      });
+      console.log((await local.root.createReader().readEntries()).length);
+      await synchronizer.synchronizeAll();
 
-    localFE = await local.root.getFile("empty.txt");
-    const remoteReader = remote.root.createReader();
-    const remoteEntries = await remoteReader.readEntries();
-    expect(remoteEntries.length).toBe(1);
-    const remoteE = remoteEntries[0];
-    expect(remoteE.isFile).toBe(true);
-    expect(remoteE.name).toBe(localFE.name);
-    expect(remoteE.fullPath).toBe(localFE.fullPath);
+      localFE = await local.root.getFile("empty.txt");
+      const remoteReader = remote.root.createReader();
+      const remoteEntries = await remoteReader.readEntries();
+      expect(remoteEntries.length).toBe(1);
+      const remoteE = remoteEntries[0];
+      expect(remoteE.isFile).toBe(true);
+      expect(remoteE.name).toBe(localFE.name);
+      expect(remoteE.fullPath).toBe(localFE.fullPath);
 
-    const localMeta = await localFE.getMetadata();
-    const remoteMeta = await remoteE.getMetadata();
-    expect(remoteMeta.size).toBe(localMeta.size);
-
-    done();
+      const localMeta = await localFE.getMetadata();
+      const remoteMeta = await remoteE.getMetadata();
+      expect(remoteMeta.size).toBe(localMeta.size);
+    } catch (e) {
+      fail(e);
+    } finally {
+      done();
+    }
   });
 
   test("add a text file, sync all", async (done) => {
-    let localFE = await local.root.getFile("test.txt", {
-      create: true,
-      exclusive: true,
-    });
-    const writer = await localFE.createWriter();
-    await writer.writeFile(new Blob(["hoge"], { type: "text/plain" }));
+    try {
+      let localFE = await local.root.getFile("test.txt", {
+        create: true,
+        exclusive: true,
+      });
+      const writer = await localFE.createWriter();
+      await writer.writeFile(new Blob(["hoge"], { type: "text/plain" }));
 
-    await synchronizer.synchronizeAll();
+      await synchronizer.synchronizeAll();
 
-    const remoteReader = remote.root.createReader();
-    const remoteEntries = await remoteReader.readEntries();
-    expect(remoteEntries.length).toBe(2);
+      const remoteReader = remote.root.createReader();
+      const remoteEntries = await remoteReader.readEntries();
+      expect(remoteEntries.length).toBe(2);
 
-    let entries = remoteEntries.filter((re) => {
-      return re.name === "empty.txt";
-    });
-    expect(entries.length).toBe(1);
-    const emptyTxt = entries[0];
-    expect(emptyTxt.isFile).toBe(true);
-    const emptyTxtMeta = await emptyTxt.getMetadata();
-    expect(emptyTxtMeta.size).toBe(0);
+      let entries = remoteEntries.filter((re) => {
+        return re.name === "empty.txt";
+      });
+      expect(entries.length).toBe(1);
+      const emptyTxt = entries[0];
+      expect(emptyTxt.isFile).toBe(true);
+      const emptyTxtMeta = await emptyTxt.getMetadata();
+      expect(emptyTxtMeta.size).toBe(0);
 
-    entries = remoteEntries.filter((re) => {
-      return re.name === "test.txt";
-    });
-    expect(entries.length).toBe(1);
-    const testTxt = entries[0];
-    expect(testTxt.isFile).toBe(true);
-    localFE = await local.root.getFile("test.txt");
-    const localFEMeta = await localFE.getMetadata();
-    const testTxtMeta = await testTxt.getMetadata();
-    expect(testTxtMeta.size).toBe(localFEMeta.size);
-
-    done();
+      entries = remoteEntries.filter((re) => {
+        return re.name === "test.txt";
+      });
+      expect(entries.length).toBe(1);
+      const testTxt = entries[0];
+      expect(testTxt.isFile).toBe(true);
+      localFE = await local.root.getFile("test.txt");
+      const localFEMeta = await localFE.getMetadata();
+      const testTxtMeta = await testTxt.getMetadata();
+      expect(testTxtMeta.size).toBe(localFEMeta.size);
+    } catch (e) {
+      fail(e);
+    } finally {
+      done();
+    }
   });
 
   test("add a hidden file, sync all", async (done) => {
-    await local.root.getFile(".hidden", {
-      create: true,
-      exclusive: true,
-    });
+    try {
+      await local.root.getFile(".hidden", {
+        create: true,
+        exclusive: true,
+      });
 
-    await synchronizer.synchronizeAll();
+      await synchronizer.synchronizeAll();
 
-    const remoteReader = remote.root.createReader();
-    const remoteEntries = await remoteReader.readEntries();
-    expect(remoteEntries.length).toBe(2);
-
-    done();
+      const remoteReader = remote.root.createReader();
+      const remoteEntries = await remoteReader.readEntries();
+      expect(remoteEntries.length).toBe(2);
+    } catch (e) {
+      fail(e);
+    } finally {
+      done();
+    }
   });
 
   test("create folder, and add a text file, sync all", async (done) => {
-    let localDE = await local.root.getDirectory("folder", {
-      create: true,
-      exclusive: true,
-    });
-    let localFE = await localDE.getFile("in.txt", {
-      create: true,
-      exclusive: true,
-    });
-    const writer = await localFE.createWriter();
-    await writer.writeFile(new Blob(["hoge"], { type: "text/plain" }));
+    try {
+      let localDE = await local.root.getDirectory("folder", {
+        create: true,
+        exclusive: true,
+      });
+      let localFE = await localDE.getFile("in.txt", {
+        create: true,
+        exclusive: true,
+      });
+      const writer = await localFE.createWriter();
+      await writer.writeFile(new Blob(["hoge"], { type: "text/plain" }));
 
-    await synchronizer.synchronizeAll();
+      await synchronizer.synchronizeAll();
 
-    const localMeta = await localFE.getMetadata();
-    const remoteDE = await remote.root.getDirectory("folder");
-    const remoteFE = await remoteDE.getFile("in.txt");
-    const remoteMeta = await remoteFE.getMetadata();
-    expect(remoteMeta.size).toBe(localMeta.size);
-
-    done();
+      const localMeta = await localFE.getMetadata();
+      const remoteDE = await remote.root.getDirectory("folder");
+      const remoteFE = await remoteDE.getFile("in.txt");
+      const remoteMeta = await remoteFE.getMetadata();
+      expect(remoteMeta.size).toBe(localMeta.size);
+    } catch (e) {
+      fail(e);
+    } finally {
+      done();
+    }
   });
 
   test("create nested folder, and add a empty file, sync dir", async (done) => {
-    const localParentDE = await local.root.getDirectory("folder");
-    const localFE = await localParentDE.getFile("fuga.txt", {
-      create: true,
-      exclusive: true,
-    });
-    let writer = await localFE.createWriter();
-    await writer.writeFile(new Blob(["fuga"], { type: "text/plain" }));
-
-    const localDE = await localParentDE.getDirectory("nested", {
-      create: true,
-      exclusive: true,
-    });
-    const nestedFE = await localDE.getFile("nested.txt", {
-      create: true,
-      exclusive: true,
-    });
-    writer = await nestedFE.createWriter();
-    await writer.writeFile(new Blob(["nested"], { type: "text/plain" }));
-
-    await synchronizer.synchronizeDirectory("/folder", false);
-
-    const localMeta = await localFE.getMetadata();
-    const remoteParentDE = await remote.root.getDirectory("folder");
-    const remoteFE = await remoteParentDE.getFile("fuga.txt");
-    const remoteMeta = await remoteFE.getMetadata();
-    expect(remoteMeta.size).toBe(localMeta.size);
-    const remoteDE = await remoteParentDE.getDirectory("nested");
-    expect(remoteDE.fullPath).toBe(localDE.fullPath);
     try {
-      await remoteDE.getFile("nested.txt");
-    } catch (e) {
-      expect(e).toBeInstanceOf(NotFoundError);
-    }
+      const localParentDE = await local.root.getDirectory("folder");
+      const localFE = await localParentDE.getFile("fuga.txt", {
+        create: true,
+        exclusive: true,
+      });
+      let writer = await localFE.createWriter();
+      await writer.writeFile(new Blob(["fuga"], { type: "text/plain" }));
 
-    done();
+      const localDE = await localParentDE.getDirectory("nested", {
+        create: true,
+        exclusive: true,
+      });
+      const nestedFE = await localDE.getFile("nested.txt", {
+        create: true,
+        exclusive: true,
+      });
+      writer = await nestedFE.createWriter();
+      await writer.writeFile(new Blob(["nested"], { type: "text/plain" }));
+
+      await synchronizer.synchronizeDirectory("/folder", false);
+
+      const localMeta = await localFE.getMetadata();
+      const remoteParentDE = await remote.root.getDirectory("folder");
+      const remoteFE = await remoteParentDE.getFile("fuga.txt");
+      const remoteMeta = await remoteFE.getMetadata();
+      expect(remoteMeta.size).toBe(localMeta.size);
+      const remoteDE = await remoteParentDE.getDirectory("nested");
+      expect(remoteDE.fullPath).toBe(localDE.fullPath);
+      try {
+        await remoteDE.getFile("nested.txt");
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundError);
+      }
+    } catch (e) {
+      fail(e);
+    } finally {
+      done();
+    }
   });
 
   test("sync dir recursively", async (done) => {
-    await synchronizer.synchronizeDirectory("/folder", true);
+    try {
+      await synchronizer.synchronizeDirectory("/folder", true);
 
-    const localNestedFE = await local.root.getFile("/folder/nested/nested.txt");
-    const localNestedMeta = await localNestedFE.getMetadata();
-    const remoteParentDE = await remote.root.getDirectory("folder");
-    const remoteDE = await remoteParentDE.getDirectory("nested");
-    const remoteNestedFE = await remoteDE.getFile("nested.txt");
-    const remoteNestedMeta = await remoteNestedFE.getMetadata();
-    expect(remoteNestedMeta.size).toBe(localNestedMeta.size);
-
-    done();
+      const localNestedFE = await local.root.getFile(
+        "/folder/nested/nested.txt"
+      );
+      const localNestedMeta = await localNestedFE.getMetadata();
+      const remoteParentDE = await remote.root.getDirectory("folder");
+      const remoteDE = await remoteParentDE.getDirectory("nested");
+      const remoteNestedFE = await remoteDE.getFile("nested.txt");
+      const remoteNestedMeta = await remoteNestedFE.getMetadata();
+      expect(remoteNestedMeta.size).toBe(localNestedMeta.size);
+    } catch (e) {
+      fail(e);
+    } finally {
+      done();
+    }
   });
 
   test("remove file, sync all", async (done) => {
