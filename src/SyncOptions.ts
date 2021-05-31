aed0a391bbcfe574603996652113be585bf821c9
Yoshihiro Seki <nikochan2k@gmail.com>
2021-05-31 18:34:20 +0900

    update kura

--- src/SyncOptions.ts
+++ src/SyncOptions.ts
@@ -1,8 +1,9 @@
-import { FileSystemObject } from "kura";
+import { FileSystemObject, Transferer } from "kura";
 
 export interface SyncOptions {
   excludeNamePattern?: string;
   excludePathPattern?: string;
   onCopy?: (from: string, to: string, obj: FileSystemObject) => Promise<void>;
+  transferer?: Transferer;
   verbose?: boolean;
 }
