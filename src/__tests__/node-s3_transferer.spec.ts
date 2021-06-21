4f0c12b40948dd9a5f8f9389d553dbb406c65326
Yoshihiro Seki <nikochan2k@gmail.com>
2021-06-21 21:32:53 +0900

    add test cases

---
+++ src/__tests__/node-s3_transferer.spec.ts
@@ -0,0 +1,57 @@
+import { S3 } from "aws-sdk";
+import { rmdirSync, statSync } from "fs";
+import { DIR_SEPARATOR } from "kura";
+import { NodeLocalFileSystemAsync, NodeTransferer } from "kura-node";
+import { S3LocalFileSystemAsync } from "kura-s3";
+import { tmpdir } from "os";
+import { normalize } from "path";
+import { testAll } from "./syncronize";
+
+testAll(async () => {
+  const tempDir = tmpdir();
+  let rootDir = `${tempDir}${DIR_SEPARATOR}web-file-system-index-test`;
+  rootDir = normalize(rootDir);
+  try {
+    statSync(rootDir);
+    rmdirSync(rootDir, { recursive: true });
+  } catch {}
+
+  const nodeFileSystem = new NodeLocalFileSystemAsync(rootDir, {
+    index: true,
+  });
+  const local = await nodeFileSystem.requestFileSystemAsync(
+    window.PERSISTENT,
+    Number.MAX_VALUE
+  );
+  await local.filesystem.accessor.purge();
+
+  const options: S3.ClientConfiguration = {
+    accessKeyId: "minioadmin",
+    secretAccessKey: "minioadmin",
+    endpoint: "http://127.0.0.1:9000",
+    s3ForcePathStyle: true, // needed with minio?
+    signatureVersion: "v4",
+  };
+  const s3 = new S3(options);
+  const bucket = "web-file-system-test";
+  try {
+    await s3.createBucket({ Bucket: bucket }).promise();
+  } catch (e) {}
+  const s3FileSystem = new S3LocalFileSystemAsync(
+    options,
+    "web-file-system-test",
+    "example",
+    {
+      index: true,
+    }
+  );
+  const remote = await s3FileSystem.requestFileSystemAsync(
+    window.PERSISTENT,
+    Number.MAX_VALUE
+  );
+  await remote.filesystem.accessor.purge();
+
+  const transferer = new NodeTransferer();
+
+  return { local, remote, transferer };
+});
